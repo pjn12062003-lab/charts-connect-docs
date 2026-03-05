a44646070ece8c3894951bdd5fe9d5c4be48449b
Vipin Kumar <vipin.ind.kumar@gmail.com>
2026-03-05 15:41:31 +0530

    favicon and tites update

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,18 +5,20 @@ import 'nextra-theme-docs/style.css'
 import type { ReactNode } from 'react'
  
 export const metadata = {
+  title: 'Charts Connect Documentation',
+  description: 'Charts Connect Documentation',
   // Define your metadata here
   // For more information on metadata API, see: https://nextjs.org/docs/app/building-your-application/optimizing/metadata
 }
  
-const banner = <Banner storageKey="some-key">Nextra 4.0 is released 🎉</Banner>
+const banner = <Banner storageKey="some-key">Charts Connect Documentation</Banner>
 const navbar = (
   <Navbar
-    logo={<b>Nextra</b>}
+    logo={<b>Charts Connect</b>}
     // ... Your additional navbar options
   />
 )
-const footer = <Footer>MIT {new Date().getFullYear()} © Nextra.</Footer>
+const footer = <Footer>{new Date().getFullYear()} © Charts Connect.</Footer>
 
 export default async function RootLayout({
   children
@@ -42,7 +44,6 @@ export default async function RootLayout({
           banner={banner}
           navbar={navbar}
           pageMap={await getPageMap()}
-          docsRepositoryBase="https://github.com/shuding/nextra/tree/main/docs"
           footer={footer}
           // ... Your additional layout options
         >
