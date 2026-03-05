5cf6123bfe7dfb6a1e2c2f3bed99f6216508b994
pjn12062003-lab <pjn12062003@gmail.com>
2026-03-05 20:04:34 +0530

    Clean up code and add new documentation pages

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,7 @@
+import "@fontsource/inter/400.css"
+import "@fontsource/inter/500.css"
+import "@fontsource/inter/600.css"
+import "@fontsource/inter/700.css"
 import { Footer, Layout, Navbar } from 'nextra-theme-docs'
 import { Banner, Head } from 'nextra/components'
 import { getPageMap } from 'nextra/page-map'
@@ -7,16 +11,11 @@ import type { ReactNode } from 'react'
 export const metadata = {
   title: 'Charts Connect Documentation',
   description: 'Charts Connect Documentation',
-  // Define your metadata here
-  // For more information on metadata API, see: https://nextjs.org/docs/app/building-your-application/optimizing/metadata
 }
  
 const banner = <Banner storageKey="some-key">Charts Connect Documentation</Banner>
 const navbar = (
-  <Navbar
-    logo={<b>Charts Connect</b>}
-    // ... Your additional navbar options
-  />
+  <Navbar logo={<b>Charts Connect</b>} />
 )
 const footer = <Footer>{new Date().getFullYear()} © Charts Connect.</Footer>
 
@@ -26,26 +25,14 @@ export default async function RootLayout({
   children: ReactNode
 }) {
   return (
-    <html
-      // Not required, but good for SEO
-      lang="en"
-      // Required to be set
-      dir="ltr"
-      // Suggested by `next-themes` package https://github.com/pacocoursey/next-themes#with-app
-      suppressHydrationWarning
-    >
-      <Head
-      // ... Your additional head options
-      >
-        {/* Your additional tags should be passed as `children` of `<Head>` element */}
-      </Head>
+    <html lang="en" dir="ltr" suppressHydrationWarning>
+      <Head />
       <body>
         <Layout
           banner={banner}
           navbar={navbar}
           pageMap={await getPageMap()}
           footer={footer}
-          // ... Your additional layout options
         >
           {children}
         </Layout>
