1416ade7e2fe1eccd186c925e12beadd8cce3ff0
pjn12062003-lab <pjn12062003@gmail.com>
2026-03-07 17:29:04 +0530

    Getting-Started Changes

--- mdx-components.js
+++ mdx-components.js
@@ -1,11 +1,13 @@
 // mdx-components.tsx
 import { useMDXComponents as getThemeMDXComponents } from 'nextra-theme-docs'
+import { Steps } from 'nextra/components'
 
 const themeComponents = getThemeMDXComponents()
 
 export function useMDXComponents(components) {
   return {
     ...themeComponents,
+    Steps,
     ...components
   }
 }
